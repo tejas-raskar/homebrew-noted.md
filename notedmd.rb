06957bc251905d10e74a946c044ddaf8b0c99b8d
Tejas Raskar <tjsrskr@gmail.com>
2025-07-01 14:27:53 +0530

    updated to v0.2.1

--- notedmd.rb
+++ notedmd.rb
@@ -1,20 +1,20 @@
 class Notedmd < Formula
   desc "A command-line tool to convert handwritten notes into clean and readable Markdown files"
   homepage "https://github.com/tejas-raskar/noted.md"
-  version "0.2.0"
+  version "0.2.1"
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/tejas-raskar/noted.md/releases/download/v0.2.0/notedmd-v0.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "db2cb7fce244e9e093b1ebe7981a85b947870d3f86813f0106f3c8acec6831fb"
+      url "https://github.com/tejas-raskar/noted.md/releases/download/v0.2.1/notedmd-v0.2.1-x86_64-apple-darwin.tar.gz"
+      sha256 "9e549157e5cfa06f1aa3b92adc3f98954a308f83de43e6c2ff07d14a6cc76b7d"
     elsif Hardware::CPU.arm?
-      url "https://github.com/tejas-raskar/noted.md/releases/download/v0.2.0/notedmd-v0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "beef1cb1e378f32ecbd9cac566c23d672efb09404211ac76c949401c0a06df78"
+      url "https://github.com/tejas-raskar/noted.md/releases/download/v0.2.1/notedmd-v0.2.1-aarch64-apple-darwin.tar.gz"
+      sha256 "65c28c781b8f1907559484f745d5c1b9a18d13a0d68b6c983091008fc7d2d18c"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/tejas-raskar/noted.md/releases/download/v0.2.0/notedmd-v0.2.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "3f7f0d3d70cd353901ed48d37d83dfe394b1b51c9a17e4fabb6f25d23dc0b6f9"
+      url "https://github.com/tejas-raskar/noted.md/releases/download/v0.2.1/notedmd-v0.2.1-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "3c22d9dec09f5dde57d2be611e5f80f103a28ad453e5df615593320f07ebdce2"
     end
   end
 
